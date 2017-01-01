df0fbdfd9b7d198f2c09dd19dcdbc35ea639c618
Laurent <lg390@cam.ac.uk>
2017-01-01 15:17:56 +0000

    fix failing test

--- tests/testthat/test_cpu.R
+++ tests/testthat/test_cpu.R
@@ -6,5 +6,5 @@ test_that("CPU www", {
                      "[Computational Proteomics Unit](http://cpu.sysbiol.cam.ac.uk/)")
     url <- gsub("^.+\\((.+)\\)", "\\1", x)
     got <- httr::GET(url)
-    expect_identical(got, stop_for_status(got))
+    expect_identical(got, httr::stop_for_status(got))
 })
