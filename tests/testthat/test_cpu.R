8fec4a12d14f07e4b85d6073fc4946bf9cc0e606
Laurent <lg390@cam.ac.uk>
2017-01-01 20:17:54 +0000

    fix test

--- tests/testthat/test_cpu.R
+++ tests/testthat/test_cpu.R
@@ -4,21 +4,19 @@ test_that("CPU www", {
     x <- cpuwww()
     expect_identical(x,
                      "[Computational Proteomics Unit](http://cpu.sysbiol.cam.ac.uk/)")
-    url <- mrkToUrl(x)
-    expect_true(validUrl(url))
 })
 
 test_that("cputools", {
     expect_null(cputools())
 })
 
-context("utils: mrkToUrl", {
+test_that("utils: mrkToUrl", {
     url0 <- "foobar"
     mrk <- "[![blabla](svg)](foobar)"
-    expect_identical(url0, mrkToUrl(mrk))    
+    expect_identical(url0, mrkToUrl(mrk))
 })
 
-context("utils: validIrl", {
+test_that("utils: validIrl", {
     expect_true(validUrl("http://www.bioconductor.org"))
     expect_true(validUrl("https://www.bioconductor.org"))
     expect_error(validUrl("foo"))
